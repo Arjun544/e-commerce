2166142480e0d5562231851aeeda1271fd9ee86a
Arjun544 <arjunsingh.mahar55@gmail.com>
2021-07-21 21:21:10 +0500

    added more api

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -8,11 +8,9 @@ exports.addToCart = async (req, res) => {
       req.body.cartItems.map(async (cartItem) => {
         let newCartItem = new CartItem({
           product: cartItem.product,
-          userId: req.body.user,
         });
 
         newCartItem = await newCartItem.save();
-
         return newCartItem._id;
       })
     ).catch(function (error) {
