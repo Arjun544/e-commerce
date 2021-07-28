4cf03a7a96e19195017b7f5dac23d2d9eb15dad3
Arjun544 <arjunsingh.mahar55@gmail.com>
2021-07-29 04:38:55 +0500

    added more fixes

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
   user: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
   review: {
     type: String,
