a1d38270628dbfa74c0957c12e6c3856b5858fb0
Arjun544 <arjunsingh.mahar55@gmail.com>
2021-09-20 23:58:20 +0500

    added cors fix

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.use(isAuthenticated());
 app.use(authError);
 app.use(cookieParser());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://sell-corner.herokuapp.com"
+  );
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
