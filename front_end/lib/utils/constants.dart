512b61af8df9ccc866f97b84adef921160e2c2ae
Arjun544 <arjunsingh.mahar55@gmail.com>
2021-11-12 19:32:07 +0500

    added fixes

--- front_end/lib/utils/constants.dart
+++ front_end/lib/utils/constants.dart
@@ -3,8 +3,8 @@ import 'package:get_storage/get_storage.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 Dio dio = Dio();
-// const String baseUrl = 'https://sell-corner.herokuapp.com/api/';
+const String baseUrl = 'https://sell-corner.herokuapp.com/api/';
 // const String baseUrl = 'http://localhost:4000/api/';
-const String baseUrl = 'http://192.168.0.107:4000/api/';
+// const String baseUrl = 'http://192.168.0.107:4000/api/';
 final GetStorage getStorage = GetStorage();
 late StreamingSharedPreferences sharedPreferences;
