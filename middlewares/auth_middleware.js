1d700feddfb55100876ef2afcf070df51f6f94e2
Arjun544 <arjunsingh.mahar55@gmail.com>
2022-03-05 22:10:30 +0500

    added fix

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -2,25 +2,21 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.headers.cookie;
-  if (!token) {
-    return res.json({
-      status: false,
-      msg: "Please provide an access token",
-    });
-  }
-
-  jwt.verify(
-    token.split("=")[1],
-    process.env.ACCESS_JWT_SECRET,
-    (err, user) => {
-      if (err) {
-        return res.json({
-          status: false,
-          msg: "Unauthorized",
-        });
+  if (!token) res.status(403).json({ error: "please provide a token" });
+  else {
+    jwt.verify(
+      token.split("=")[1],
+      process.env.ACCESS_JWT_SECRET,
+      (err, user) => {
+        if (err) {
+          return res.json({
+            status: false,
+            msg: "Unauthorized",
+          });
+        }
+        req.user = user;
+        next();
       }
-      req.user = user;
-      next();
-    }
-  );
+    );
+  }
 };
