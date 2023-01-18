34dcc49a73962ba6799d950dace0b2e757733020
arjun544 <arjunsingh.mahar55@gmail.com>
2023-01-18 15:04:15 +0500

    Upgraded dependencies

--- config/db_config.js
+++ config/db_config.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
+    mongoose.set("strictQuery", false);
+    console.log(process.env.DB_URL);
+
+    mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
     });
+    mongoose.Promise = global.Promise;
     console.log("Connected to DB..!");
   } catch (error) {
     process.exit(1);
