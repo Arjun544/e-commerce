4b20ec84202ebe5b55e72d1a26a508a4576e9389
arjun544 <arjunsingh.mahar55@gmail.com>
2023-01-30 18:47:36 +0500

    Added API url for mobile app

--- front_end/lib/utils/constants.dart
+++ front_end/lib/utils/constants.dart
@@ -3,8 +3,6 @@ import 'package:get_storage/get_storage.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 Dio dio = Dio();
-// const String baseUrl = 'e-commerce-production-45e7.up.railway.app/api/';
-// const String baseUrl = 'http://192.168.43.29:4000/api/';
-const String baseUrl = 'http://192.168.43.29:4000/api/';
+const String baseUrl = 'https://e-commerce-production-45e7.up.railway.app/api/';
 final GetStorage getStorage = GetStorage();
 late StreamingSharedPreferences sharedPreferences;
